508e66057f6e5a7784fe68452464e2fff26c505a
Allison Levine <allison.s.levine@gmail.com>
2021-01-27 02:36:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var greeting = "Hello, everybody!"
 var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = `Hello, ${specialGuest}!`
-var topic = 
+var topic = "doodies"
 var conversation = `Let's talk about ${topic}.`
 greeting
 greetSpecialGuest
+conversation
+
